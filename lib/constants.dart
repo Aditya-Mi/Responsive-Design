de6a1b36b38660aebf69648870313e86e007caff
Aditya Mittal <adityamittal0918@gmail.com>
2023-10-27 18:02:38 +0530

    .

--- lib/constants.dart
+++ lib/constants.dart
@@ -12,70 +12,72 @@ class Styles {
 
 var drawer = Drawer(
   backgroundColor: color3,
-  child: Column(
-    children: [
-      const DrawerHeader(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 40,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Name',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+  child: SingleChildScrollView(
+    child: Column(
+      children: [
+        const DrawerHeader(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 40,
               ),
-            ),
-          ],
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      ListTile(
-        leading: const Icon(
-          Icons.home_rounded,
-          color: Colors.white,
+        ListTile(
+          leading: const Icon(
+            Icons.home_rounded,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Home',
+            style: Styles.drawerTextStyle(),
+          ),
         ),
-        title: Text(
-          'Home',
-          style: Styles.drawerTextStyle(),
+        ListTile(
+          leading: const Icon(
+            Icons.person,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Profile',
+            style: Styles.drawerTextStyle(),
+          ),
         ),
-      ),
-      ListTile(
-        leading: const Icon(
-          Icons.person,
-          color: Colors.white,
+        ListTile(
+          leading: const Icon(
+            Icons.settings_rounded,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Settings',
+            style: Styles.drawerTextStyle(),
+          ),
         ),
-        title: Text(
-          'Profile',
-          style: Styles.drawerTextStyle(),
+        ListTile(
+          leading: const Icon(
+            Icons.logout_rounded,
+            color: Colors.white,
+          ),
+          title: Text(
+            'Logout',
+            style: Styles.drawerTextStyle(),
+          ),
         ),
-      ),
-      ListTile(
-        leading: const Icon(
-          Icons.settings_rounded,
-          color: Colors.white,
-        ),
-        title: Text(
-          'Settings',
-          style: Styles.drawerTextStyle(),
-        ),
-      ),
-      ListTile(
-        leading: const Icon(
-          Icons.logout_rounded,
-          color: Colors.white,
-        ),
-        title: Text(
-          'Logout',
-          style: Styles.drawerTextStyle(),
-        ),
-      ),
-    ],
+      ],
+    ),
   ),
 );
